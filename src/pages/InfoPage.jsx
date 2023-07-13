5961a3d8d3c7e0dc0b3d2047069a6640a209d936
bavichugo <victorhugobatistela@gmail.com>
2023-07-12 23:12:10 -0700

    Fix messages on info page

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -2,36 +2,35 @@ const InfoPage = () => {
   return (
     <div className="flex flex-col w-full max-w-3xl gap-4 mx-4">
       <div className="flex flex-col w-full bg-[#283147] p-4 rounded-xl">
-        <span className="underline underline-offset-4">How to use</span>
+        <span className="underline underline-offset-4 mb-2">How to use</span>
         <span>
           - On the main box, add the expense name, your friends name and their
-          items, and finally the total + tax + tips.
+          items,and the total + tax + tips. Than, press calculate and the total
+          each person should pay will be calculated.
         </span>
         <br />
         <span>
-          NOTE: it is very important to add the correct TOTAL + TAX + TIPS
-          value, which is the value that will be at the bottom of your receipt,
-          and not the total of the items without taxes and tips. That will
-          ensure that the taxes and tips are added to the total of each member
-          at the end of the calculation.
+          NOTE: Ensure that you use the total amount with tax and tips included
+          to accurately calculate each member's share, rather than using the
+          subtotal without tax and tips. This will ensure that taxes and tips
+          are properly accounted for in each member's calculation.
         </span>
       </div>
       <div className="flex flex-col w-full bg-[#283147] p-4 rounded-xl">
-        <span className="underline underline-offset-4">
+        <span className="underline underline-offset-4 mb-2">
           How is the total per person calculated
         </span>
-        <span>
-          - The formula to calculate how much each person should pay is
-        </span>
-        <br />
-        <span>((Person Total) / (Meal Total)) * (Total + Tax + Tips)</span>
-        <br />
-        <span>
-          The first part ((Person Total) / (Meal Total)) calculates the
-          percentage each person is responsible for paying of the meal. Finally,
-          we multiply that percentage by the (Total + Tax + Tips) to get the
-          real value each person should pay.
-        </span>
+        <ol>
+          <li>
+            1. Divide the person's total by the meal total to determine their
+            percentage of responsibility: (Person Total) / (Meal Total).
+          </li>
+          <li>
+            2. Multiply the percentage from step 1 by the sum of the total, tax,
+            and tips: (Total + Tax + Tips).
+          </li>
+          <li>3. The result is the amount each person should pay.</li>
+        </ol>
       </div>
     </div>
   );
